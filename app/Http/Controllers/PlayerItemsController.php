cdc54086093a8d4d38bf94172713f090450ce999
CieL2731 <kuroneko7205@gmail.com>
2023-10-06 10:51:02 +0900

    ファイル名変更

--- app/Http/Controllers/PlayerItemsController.php
+++ app/Http/Controllers/PlayerItemsController.php
@@ -12,7 +12,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Log;
 
-class ItemController extends Controller
+class PlayerItemsController extends Controller
 {
     /**
      * Store or add items to a resource
